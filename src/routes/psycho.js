334d959e0766a12eea091b3772b2c8286bffaf5a
belfortep <pbelforte2003@gmail.com>
2022-05-21 13:23:51 -0300

    created verifyToken util

--- src/routes/psycho.js
+++ src/routes/psycho.js
@@ -1,13 +1,13 @@
 const Router = require('express');
 const { getAllPsychos, createPsycho, getPsychoById, deletePsycho, updatePsycho } = require('../controllers/psychoController');
 const router = Router();
+const { verifyUser } = require('../util/verifyToken');
 
-
-router.get('/', getAllPsychos);
-router.post('/', createPsycho);
-router.get('/:id', getPsychoById);
-router.delete('/:id', deletePsycho);
-router.put('/:id', updatePsycho);
+router.get('/', verifyUser, getAllPsychos);
+router.post('/', verifyUser, createPsycho);
+router.get('/:id', verifyUser, getPsychoById);
+router.delete('/:id', verifyUser, deletePsycho);
+router.put('/:id', verifyUser, updatePsycho);
 
 module.exports = router
 
--- src/routes/solid.js
+++ src/routes/solid.js
@@ -1,12 +1,12 @@
 const Router = require('express')
 const router = Router();
 const { getAllSolids, createSolid, getSolidById, deleteSolid, updateSolid } = require('../controllers/solidController');
-
-router.get('/', getAllSolids);
-router.post('/', createSolid);
-router.get('/:id', getSolidById);
-router.delete('/:id', deleteSolid);
-router.put('/:id', updateSolid);
+const { verifyUser } = require('../util/verifyToken');
+router.get('/', verifyUser, getAllSolids);
+router.post('/', verifyUser, createSolid);
+router.get('/:id', verifyUser, getSolidById);
+router.delete('/:id', verifyUser, deleteSolid);
+router.put('/:id', verifyUser, updateSolid);
 
 module.exports = router
 
